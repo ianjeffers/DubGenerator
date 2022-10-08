3f338463eb24b1cc8b0b3314701b03673a19d9d1
ianjeffers <71468572+ianjeffers@users.noreply.github.com>
2022-10-07 21:39:30 -0700

    Version 1.0 of Language Dub Generator

---
+++ main.py
@@ -0,0 +1,56 @@
+from gtts import gTTS
+from googletrans import Translator
+
+import speech_recognition as sr
+import pyttsx3
+
+# Initialize the recognizer
+r = sr.Recognizer()
+translator = Translator()
+
+
+# Function to convert text to
+# speech
+def SpeakText(command):
+    # Initialize the engine
+    engine = pyttsx3.init()
+    engine.say(command)
+    engine.runAndWait()
+
+
+# Loop infinitely for user to
+# speak
+
+
+
+while (1):
+    # Exception handling to handle
+    # exceptions at the runtime
+    try:
+
+        # use the microphone as source for input.
+        with sr.Microphone() as source2:
+
+            # wait for a second to let the recognizer
+            # adjust the energy threshold based on
+            # the surrounding noise level
+            r.adjust_for_ambient_noise(source2, duration=0.2)
+
+            # listens for the user's input
+            audio2 = r.listen(source2)
+
+            # Using google to recognize audio
+            recorded_text = r.recognize_google(audio2)
+            recorded_text = recorded_text.lower()
+            print("Recorded text:\n",recorded_text)
+            translated_text = translator.translate(recorded_text, src='en', dest=language)
+            print("Translated text:\n", translated_text)
+
+            myobj = gTTS(text=recorded_text, lang='de', slow=False)
+            myobj.save("TestGerman.mp3")
+
+    except sr.RequestError as e:
+        print("Could not request results; {0}".format(e))
+
+    except sr.UnknownValueError:
+        print("No Audio")
